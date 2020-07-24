48ab607b44964e29fc636e9542e1a0d2dfeb3b68
mikewise2718 <mwise@microsoft.com>
2020-07-24 20:02:02 +0200

    added firefly panel

---
+++ Assets/_scripts/Ui/FireFlyPanel.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CampusSimulator;
+
+public class FireFlyPanel : MonoBehaviour
+{
+    public SceneMan sman;
+    FrameMan fman;
+    UiMan uiman;
+
+    Toggle fastModeToggle;
+    Toggle useDfInexesToggle;
+
+    Text SimpleDfIndexCountText;
+
+    Button closeButton;
+
+    public void Init0()
+    {
+        LinkObjectsAndComponents();
+    }
+
+    public void LinkObjectsAndComponents()
+    {
+        sman = FindObjectOfType<SceneMan>();
+        uiman = sman.uiman;
+        fman = sman.frman;
+
+        fastModeToggle = transform.Find("FastModeToggle").gameObject.GetComponent<Toggle>();
+        useDfInexesToggle = transform.Find("UseDataFileIndexesToggle").gameObject.GetComponent<Toggle>();
+        SimpleDfIndexCountText = transform.Find("SimpleDfIndexCountText").gameObject.GetComponent<Text>();
+
+        closeButton = transform.Find("CloseButton").gameObject.GetComponent<Button>();
+        closeButton.onClick.AddListener(delegate { uiman.ClosePanel();  });
+
+    }
+    public void InitVals()
+    {
+        Debug.Log("GeneralPanel InitVals called");
+        if (fastModeToggle != null)
+        {
+            fastModeToggle.isOn = sman.fastMode;
+        }
+    }
+
+    public void SetScene(CampusSimulator.SceneSelE curscene)
+    {
+    }
+    public void UpdateText()
+    {
+        var tx = "";
+        var dfman = sman.dfman;
+        var (rv1, rv2, rv3) = dfman.GetIndexCounts();
+        tx = $"SimpleDf Index Counts - Ways:{rv1} Links:{rv2} Nodes:{rv3}";
+        SimpleDfIndexCountText.text = tx;
+    }
+
+    public void SetVals(bool closing = false)
+    {
+        Debug.Log($"GeneralPanel.SetVals called - closing:{closing}");
+        sman.fastMode = fastModeToggle.isOn;
+        sman.dfman.useDfIndexes.SetAndSave(useDfInexesToggle.isOn);
+
+        sman.RequestRefresh("GeneralPanel-SetVals");
+    }
+
+    float checkInterval = 1f;
+    float lastCheck = 0;
+
+    private void Update()
+    {
+        if (Time.time-lastCheck>checkInterval)
+        {
+            UpdateText();
+            lastCheck = Time.time;
+        }
+    }
+
+}
